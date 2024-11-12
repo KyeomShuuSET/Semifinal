52be50a7cdc51c2b61c222ba31e04eb70fcb0e09
KyeomShuuSET <frenzy.maemagadia@gmail.com>
2024-11-12 17:10:26 +0800

    Update Express.js

--- Express.js
+++ Express.js
@@ -26,27 +26,27 @@ const connection = mysql.createConnection({
 connection.connect();
 
 app.get("/api/products", (req, res) => {
-    connection.query("SELECT * FROM productsb ", (err,rows, fields) => {
+    connection.query("SELECT * FROM products_infob ", (err,rows, fields) => {
         if(err) throw err;
         res.json(rows)
     })
 })
 
 
-//REPORT - CRUD - SEARCH 
-// app.get("/api/products/:id", (req, res) => {
-//     const id=req.params.id
-//     //res.send(id)
-//     connection.query(`SELECT * FROM productsb WHERE id=${id}`,(err, row, fields) => {
-//         if(err) throw err
-//         if(row.length > 0){
-//             res.json(rows)
-//         }else{
-//             res.status(400).json({msg:`${id} id not found`})
-//         }
-//         res.json(rows)
-//     })
-// })
+REPORT - CRUD - SEARCH 
+app.get("/api/products/:id", (req, res) => {
+    const id=req.params.id
+    //res.send(id)
+    connection.query(`SELECT * FROM products_infob WHERE id=${id}`,(err, row, fields) => {
+        if(err) throw err
+        if(row.length > 0){
+            res.json(rows)
+        }else{
+            res.status(400).json({msg:`${id} id not found`})
+        }
+        res.json(rows)
+    })
+})
 
 //POST
 //CREATE - CRUD
@@ -63,43 +63,6 @@ app.post("/api/products"), (req, res) => {
 }
 
 
-    // const fname = req.body.fname; //Vernon
-    // const lname = req.body.lname; //Chwe
-    // const email = req.body.email; //Vernon@gmail.com
-    // const gender = req.body.gender; //beki
-//     connection.query(`INSERT INTO userdata (first_name, last_name, email, gender) VALUES ('${fname}','${lname}', '${email}', '${gender}')`,(err, rows, fields) => {
-//         if(err) throw err;
-//         res.json({msg: `Successfully inserted`})
-//     })
-// })
-
-//PUT
-//UPDATE - CRUD
-// app.use(express.urlencoded({extended: false}))
-// app.put("/api/products", (req,res) => {
-//     const id=req.body.id;
-//     const itemName=req.body.itemName;
-//     const unitPrice=req.body.unitPrice;
-//     const quantity= req.body.quantity;
-//     connection.query(`UPDATE productsb set itemName='${itemName}', unitPrice='${unitPrice}', quantity='${quantity}' WHERE id=${id}'`, (err, rows, fields)=>{
-//         if(err) throw err;
-//         res.json({msg:`Sucessfully updated!`})
-//     })
-// }),
-
-
-
-
-
-//     const fname = req.body.fname; //Vernon
-//     const lname = req.body.lname; //Chwe
-//     const email = req.body.email; //Vernon@gmail.com
-//     const gender = req.body.gender; //beki
-//     connection.query(`UPDATE userdata SET first_name='${fname}', last_name='${lname}',email='${email}', gender='${gender}' WHERE id=${id}' ` , (err,rows,fields) => {
-//         if(err) throw err;
-//         res.json({msg: `Successfully updated!`})
-//     })
-// })
 
 
 
